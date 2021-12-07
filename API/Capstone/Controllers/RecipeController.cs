157947eb03bad35a5110b8d3e59584d81c4df26f
Alec Steines <alecsteines@gmail.com>
2021-12-07 12:48:45 -0500

    All recipes get added, interfaces/daos/models started, controllers added

---
+++ API/Capstone/Controllers/RecipeController.cs
@@ -0,0 +1,33 @@
+﻿using Capstone.DAO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRecipeDao recipeDao;
+
+        public RecipeController(IRecipeDao _recipeDao)
+        {
+            recipeDao = _recipeDao;
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAllRecipes()
+        {
+            List<string> allRecipes = recipeDao.GetAllRecipes();
+            if(allRecipes!=null)
+            {
+                return Ok(allRecipes);
+            }
+            return StatusCode(503);
+        }
+    }
+}
