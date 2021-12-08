1f939585de57fe4c80b6ef5365adc06f6d12d681
Henry Brooks <henrybrooksp@gmail.com>
2021-12-07 23:17:49 -0500

    Recipe service and all recipes component

---
+++ vue/src/components/RecipesList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <div class = "recipes">
+            <div
+            class="recipe" 
+            v-for="recipe in recipes"
+            v-bind:key="recipe.recipeId">
+            <h1>{{recipe.recipeName}}</h1>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import recipeService from '../services/RecipeService';
+export default {
+    data(){
+        return{
+            recipes:[],
+            title: "All Recipes"
+        };
+    },
+    created(){
+        recipeService.getRecipes().then(response =>{
+            this.recipes = response.data;
+        });
+        console.log("after get");
+    }
+};
+</script>
