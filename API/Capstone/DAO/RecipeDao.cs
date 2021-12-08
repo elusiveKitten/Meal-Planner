061a3dc0eaa3476faae33056537ec807698eaef9
Alec Steines <alecsteines@gmail.com>
2021-12-08 11:33:32 -0500

    Added search by ingredient, get recipes by user, made all recipes yield more properties

--- API/Capstone/DAO/RecipeDao.cs
+++ API/Capstone/DAO/RecipeDao.cs
@@ -15,7 +15,42 @@ namespace Capstone.DAO
         {
             connectionString = dbConnectionString;
         }
+        public List<MealRecipe> GetRecipesByUser(int userId)
+        {
+            List<MealRecipe> userRecipes = new List<MealRecipe>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
 
+                    SqlCommand cmd = new SqlCommand("SELECT recipes.recipe_id, recipe_name, calories, instructions, category_name, dish_type_name  FROM recipes " +
+                        "JOIN recipe_category ON recipes.recipe_id = recipe_category.recipe_id " +
+                        "JOIN recipe_dish_type ON recipes.recipe_id = recipe_dish_type.recipe_id " +
+                        "JOIN category ON recipe_category.category_id = category.category_id " +
+                        "JOIN dish_type ON recipe_dish_type.dish_type_id = dish_type.dish_type_id " +
+                        "JOIN recipes_users ON recipes.recipe_id = recipes_users.recipe_id " +
+                        "JOIN users ON recipes_users.user_id = users.user_id " +
+                        "WHERE users.user_id = @userId", conn);
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        MealRecipe mealRecipe = GetMealRecipeFromReader(reader);
+                        if(mealRecipe != null)
+                        {
+                            userRecipes.Add(mealRecipe);
+                        }
+                    }
+                    reader.Close();
+                }
+                return userRecipes;
+            }
+            catch (SqlException)
+            {
+                throw new Exception();
+            }
+        }
         public MealRecipe GetRecipe(int recipeId)
         {
             MealRecipe mealRecipe = new MealRecipe();
@@ -25,7 +60,7 @@ namespace Capstone.DAO
                 {
                     conn.Open();
 
-                    SqlCommand cmd = new SqlCommand("SELECT recipes.recipe_id, recipe_name, instructions, category_name, dish_type_name  FROM recipes " +
+                    SqlCommand cmd = new SqlCommand("SELECT recipes.recipe_id, recipe_name, calories, instructions, category_name, dish_type_name  FROM recipes " +
                         "JOIN recipe_category on recipes.recipe_id = recipe_category.recipe_id " +
                         "JOIN recipe_dish_type on recipes.recipe_id = recipe_dish_type.recipe_id " +
                         "JOIN category on recipe_category.category_id = category.category_id " +
@@ -47,20 +82,24 @@ namespace Capstone.DAO
             }
         }
 
-        public List<Recipe> GetAllRecipes()
+        public List<MealRecipe> GetAllRecipes()
         {
-            List<Recipe> recipes = new List<Recipe>();
+            List<MealRecipe> recipes = new List<MealRecipe>();
             try
             {
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
                     conn.Open();
 
-                    SqlCommand cmd = new SqlCommand("SELECT recipe_id, recipe_name FROM recipes", conn);
+                    SqlCommand cmd = new SqlCommand("SELECT recipes.recipe_id, recipe_name, calories, instructions, category_name, dish_type_name  FROM recipes " +
+                        "JOIN recipe_category on recipes.recipe_id = recipe_category.recipe_id " +
+                        "JOIN recipe_dish_type on recipes.recipe_id = recipe_dish_type.recipe_id " +
+                        "JOIN category on recipe_category.category_id = category.category_id " +
+                        "JOIN dish_type on recipe_dish_type.dish_type_id = dish_type.dish_type_id", conn);
                     SqlDataReader reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
-                        Recipe recipe = GetRecipeFromReader(reader);
+                        MealRecipe recipe = GetMealRecipeFromReader(reader);
                         if (recipe != null)
                         {
                             recipes.Add(recipe);
@@ -75,22 +114,40 @@ namespace Capstone.DAO
                 throw new Exception();
             }
         }
-        private Recipe GetRecipeFromReader(SqlDataReader reader)
+        public List<MealRecipe> SearchByIngredient(string ingredient)
         {
+            List<MealRecipe> recipes = new List<MealRecipe>();
             try
             {
-                Recipe recipe = new Recipe()
+                using (SqlConnection conn = new SqlConnection(connectionString))
                 {
-                    RecipeId = Convert.ToInt32(reader["recipe_id"]),
-                    RecipeName = Convert.ToString(reader["recipe_name"]),
-                };
-                return recipe;
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT recipes.recipe_id, recipe_name, calories, instructions, category_name, dish_type_name FROM recipes " +
+                        "JOIN recipe_category on recipes.recipe_id = recipe_category.recipe_id " +
+                        "JOIN recipe_dish_type on recipes.recipe_id = recipe_dish_type.recipe_id " +
+                        "JOIN category on recipe_category.category_id = category.category_id " +
+                        "JOIN dish_type on recipe_dish_type.dish_type_id = dish_type.dish_type_id " +
+                        "JOIN recipe_ingredients ON recipes.recipe_id = recipe_ingredients.recipe_id " +
+                        "JOIN ingredients ON recipe_ingredients.ingredient_id = ingredients.ingredient_id " +
+                        "WHERE ingredients.ingredient_name LIKE '%" + ingredient + "%'", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        MealRecipe recipe = GetMealRecipeFromReader(reader);
+                        if (recipe != null)
+                        {
+                            recipes.Add(recipe);
+                        }
+                    }
+                    reader.Close();
+                }
+                return recipes;
             }
             catch (SqlException)
             {
                 throw new Exception();
             }
-
         }
         private MealRecipe GetMealRecipeFromReader(SqlDataReader reader)
         {
@@ -101,6 +158,7 @@ namespace Capstone.DAO
                     //recipes.recipe_id could be an issue in the future!! Otherwise it may be the fix!
                     RecipeId = Convert.ToInt32(reader["recipe_id"]),
                     RecipeName = Convert.ToString(reader["recipe_name"]),
+                    Calories = Convert.ToString(reader["calories"]),
                     Instructions = Convert.ToString(reader["instructions"]),
                     Category = Convert.ToString(reader["category_name"]),
                     DishType = Convert.ToString(reader["dish_type_name"])
