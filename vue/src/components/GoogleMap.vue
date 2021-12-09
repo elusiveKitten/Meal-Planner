70f59e8048723bc98f0fb05f6b4a0cabf4a0f6fa
Nanette Belli <nanettebelli@gmail.com>
2021-12-09 13:48:19 -0500

    googlemap vue started

---
+++ vue/src/components/GoogleMap.vue
@@ -0,0 +1,40 @@
+<!--added for googlemap function-->
+<template>
+  <div>
+    <div style="display: flex; align-items: center; justify-content: space-between"></div>
+     <h1> Your coordinates:</h1>
+     <p>{{ coordinates.lat}} Latitude, {{ coordinates.lng}} Longitude</p>
+     <GmapMap
+      :center="{lat:10, lng:10}"
+      :zoom="7"
+      style="width:640px; height:360px; margin: 32px; auto;"
+      ></GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "GoogleMap",
+    data() {
+      return {
+        coordinates:{
+          lat: 0,
+          lng: 0
+        }
+      }
+    },
+    created() {
+      //get user's coordinates from browser request
+      this.$getLocation({})
+      .then(coordinates => {
+        this.coordinates = coordinates;
+      })
+      .catch(error => alert(error));
+    }
+   
+}
+</script>
+
+<style>
+
+</style>
